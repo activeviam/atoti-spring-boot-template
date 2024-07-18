8428f9fde927f613f3d1f3aee38dcf1110c27db0
Olivier Billiard <obi@activeviam.com>
2024-07-18 16:34:50 +0800

    Review the custom rest endpoint and protect their access as well as Swagger-UI

---
+++ src/main/java/com/activeviam/apps/rest/EndpointConstants.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) ActiveViam 2024
+ * ALL RIGHTS RESERVED. This material is the CONFIDENTIAL and PROPRIETARY
+ * property of ActiveViam Limited. Any unauthorized use,
+ * reproduction or transfer of this material is strictly prohibited
+ */
+
+package com.activeviam.apps.rest;
+
+import com.activeviam.web.core.internal.rest.RestPrefixExtractor;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EndpointConstants {
+    public static final String CUSTOM_REST_PATH = "/custom/" + RestPrefixExtractor.REST_NAMESPACE;
+}
