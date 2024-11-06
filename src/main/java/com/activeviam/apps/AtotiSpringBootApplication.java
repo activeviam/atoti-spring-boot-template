6a22c0607ebb15c27b27e535bcca80bc0fb5293d
Olivier Billiard <obi@activeviam.com>
2024-11-06 14:47:55 +0800

    Simplified OTEL which works in degraded mode

--- src/main/java/com/activeviam/apps/AtotiSpringBootApplication.java
+++ src/main/java/com/activeviam/apps/AtotiSpringBootApplication.java
@@ -11,30 +11,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import com.activeviam.tech.core.api.tracking.Tracing;
-
-import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
-
-@SpringBootApplication( // temporary see https://activeviam.atlassian.net/issues/PIVOT-10272
-        exclude = {
-            org.springframework.boot.actuate.autoconfigure.tracing.OpenTelemetryAutoConfiguration.class,
-            org.springframework.boot.actuate.autoconfigure.opentelemetry.OpenTelemetryAutoConfiguration.class
-        })
+@SpringBootApplication
 @EnableWebMvc
 @ConfigurationPropertiesScan
 public class AtotiSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AtotiSpringBootApplication.class, args);
-        OpenTelemetryAppender.install(Tracing.getEffectiveOtelInstance());
     }
-
-    // FIXME does not set up the trace id in the logs when using this method
-    //    @Bean
-    //    @Order(Integer.MIN_VALUE)
-    //    OpenTelemetry openTelemetry() {
-    //        var openTelemetry = GlobalOpenTelemetry.get();
-    //        OpenTelemetryAppender.install(openTelemetry);
-    //        return openTelemetry;
-    //    }
 }
