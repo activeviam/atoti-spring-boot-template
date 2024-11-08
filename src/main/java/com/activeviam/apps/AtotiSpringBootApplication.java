cd76a2f7798932be9beca9f7be793e9839b7689d
nidabouzid <nbo@activeviam.com>
2024-11-08 12:02:29 +0100

    issue when agent is used, workaround for now

--- src/main/java/com/activeviam/apps/AtotiSpringBootApplication.java
+++ src/main/java/com/activeviam/apps/AtotiSpringBootApplication.java
@@ -9,7 +9,12 @@ package com.activeviam.apps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 
 @SpringBootApplication
 @EnableWebMvc
@@ -18,5 +23,11 @@ public class AtotiSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AtotiSpringBootApplication.class, args);
+    }
+    // FIXME https://activeviam.atlassian.net/browse/PIVOT-10624
+    @Bean
+    @Order(Integer.MIN_VALUE)
+    OpenTelemetry openTelemetry() {
+        return GlobalOpenTelemetry.get();
     }
 }
