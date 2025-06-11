a346bc144b1093d5e049b5175dfe8ac79ce5dc69
Marco Fabiani <mfa@activeviam.com>
2025-06-11 18:18:23 +0100

    shift using analysis hierarchy

--- src/main/java/com/activeviam/apps/cfg/pivot/datanode/Measures.java
+++ src/main/java/com/activeviam/apps/cfg/pivot/datanode/Measures.java
@@ -21,6 +21,7 @@ import java.util.function.Consumer;
 import com.activeviam.activepivot.copper.api.Copper;
 import com.activeviam.activepivot.copper.api.CopperMeasure;
 import com.activeviam.activepivot.core.intf.api.copper.ICopperContext;
+import com.activeviam.database.api.types.ILiteralType;
 
 public class Measures implements Consumer<ICopperContext> {
     private final List<CopperMeasure> copperMeasures = new ArrayList<>();
@@ -59,6 +60,14 @@ public class Measures implements Consumer<ICopperContext> {
                     var shiftCobDate = w.read(1);
                     w.write(0, shiftCobDate);
                 }));
-        return underlying.minus(previous);
+        return Copper.combine(underlying, previous)
+                .map(
+                        (r, w) -> {
+                            if (r.isNull(0) || r.isNull(1)) {
+                                w.writeNull();
+                            }
+                            w.write(r.readDouble(0) - r.readDouble(1));
+                        },
+                        ILiteralType.DOUBLE);
     }
 }
